822e6f6d070162654f4789c3e9fafc80168b3ad0
user <shantokhan929339@gmail.com>
2024-05-10 22:21:38 +0600

    s

--- lib/View/Screens/Pdf_generate_page.dart
+++ lib/View/Screens/Pdf_generate_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter/services.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
+import 'package:intl/intl.dart';
+
 
 class PdfPage extends StatefulWidget {
   const PdfPage({super.key});
@@ -13,78 +15,107 @@ class PdfPage extends StatefulWidget {
 }
 
 class _PdfPageState extends State<PdfPage> {
-
+  DateTime now=DateTime.now();
+  getDate(){
+     now = DateTime.now();
+  }
+  //String formattedDate = DateFormat('kk:mm:ss \n EEE d MMM').format(now);
   final Random _random =
-  Random(); // Create a Random object for generating random numbers
+      Random(); // Create a Random object for generating random numbers
   int _randomNumber = 0;
   @override
   void initState() {
     setState(() {
-      _randomNumber =
-          1000 + _random.nextInt(9000);
+      getDate();
+      _randomNumber = 1000 + _random.nextInt(9000);
     });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Container(
-        height:double.maxFinite,
+        height: double.maxFinite,
         width: double.maxFinite,
         child: PdfPreview(
-          build: (format) => _generatePdf( "Test "),
+          build: (format) => _generatePdf(),
         ),
       ),
     );
   }
 
-  Future<Uint8List> _generatePdf( String title) async {
+  ///<<<<<<<<<<<<<<<< this part is design pdf >>>>>>>>>>>>>>>>>>
+
+  Future<Uint8List> _generatePdf() async {
     final pdf = pw.Document(version: PdfVersion.pdf_1_5, compress: true);
     final font = await PdfGoogleFonts.nunitoExtraLight();
-
     pdf.addPage(
       pw.Page(
         pageFormat: PdfPageFormat.a4,
         build: (context) {
           return pw.Column(
             children: [
-                pw.Row(
-                  mainAxisAlignment:pw.MainAxisAlignment.spaceBetween,
-                  children: [
-                    pw.Column(
-                      crossAxisAlignment:pw.CrossAxisAlignment.start,
-                    children:[
-                    pw.Text(
-                     "Invoice",
-                      style: pw.TextStyle(
-                       fontSize: 24,
-                        fontWeight: pw.FontWeight.bold
-                      )
-                    ),
-                    pw.Text(
-                    "#${_randomNumber.toString()}",
-                     style: const pw.TextStyle(
-                      fontSize:12,
-                     )
-                    )
-                    ]
-                    ),
-                    pw.Align(
-                     alignment:pw.AlignmentDirectional.centerEnd,
-                      child:pw.SizedBox(
-                        height: 80,
-                        width: 80,
-                        child: pw.FlutterLogo()),
-                    ),
-                  ],
-                ),
-              pw.SizedBox(
-                height: 10
+              pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                children: [
+                  pw.Column(
+                      crossAxisAlignment: pw.CrossAxisAlignment.start,
+                      children: [
+                        pw.Text("Invoice",
+                            style: pw.TextStyle(
+                                fontSize: 24, fontWeight: pw.FontWeight.bold)),
+                        pw.SizedBox(height:10),
+                    ///<<<<<<<<<<<<<<<<<<<<<<<<<< This is for date section >>>>>>>>>>>>>>>>>>>>>>>>
+                        pw.Row(
+                         children: [
+                           pw.Text(
+                               "Date: ",
+                             style: pw.TextStyle(
+                               fontSize: 14,
+                             fontWeight: pw.FontWeight.bold,
+                             ),
+                           ),
+
+                           pw.Text(
+                            DateFormat("yMMMd").format(now)
+                           )
+
+                         ]
+                        ),
+                        pw.SizedBox(height:8),
+
+                        pw.Row(
+                          children: [
+
+                            pw.Text(
+                              "Invoice",
+                              style: pw.TextStyle(
+                               fontSize: 14,
+                               fontWeight: pw.FontWeight.bold
+                              )
+                            ),
+
+                            pw.Text(": #${_randomNumber.toString()}",
+                                style: const pw.TextStyle(
+                                  fontSize: 12,
+                                )),
+                          ],
+                        )
+                      ]),
+                  pw.Align(
+                    alignment: pw.AlignmentDirectional.centerEnd,
+                    child: pw.SizedBox(
+                        height: 80, width: 80, child:
+                        //pw.Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL3kmkFtmxjnWrKc3LDY289a0UroPToiMdTQ&s"),
+                    pw.FlutterLogo()),
+
+                  ),
+                ],
               ),
-             pw.Divider(
-               height: 1
-             ),
+              pw.SizedBox(height: 16),
+              pw.Divider(height: 1),
+
             ],
           );
         },
@@ -94,4 +125,3 @@ class _PdfPageState extends State<PdfPage> {
     return pdf.save();
   }
 }
-
